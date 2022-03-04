2857a63d6b43e7666963e1fa1c083b6df5a2429a
Zhangeldi Smagulov <zhan2301@gmail.com>
2022-03-04 18:17:34 +0600

    server

--- server/router/router.go
+++ server/router/router.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gorilla/mux"
 
 	//"real-estate1/middleware"
+
+	"real-estate1/middleware"
 )
 
 // Router is exported and used in main.go
@@ -11,9 +13,7 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	/*router.HandleFunc("/api/task", middleware.GetAllTask).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/task", middleware.CreateTask).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")*/
 	return router
 	//dfd
 }
