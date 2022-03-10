0a228ee6e7a8cfe2df801e3645a4c45c9012cddb
Almaz <63015341+Morr231@users.noreply.github.com>
2022-03-10 23:14:41 +0600

    fetch

--- client/src/search-in-offers/search-in-offers-comp/search-in-offers-cards.js
+++ client/src/search-in-offers/search-in-offers-comp/search-in-offers-cards.js
@@ -4,6 +4,18 @@ import RealEstateCard from "../../real-estate-card/real-estate-card";
 import "../search-in-offers-sass/search-in-offers-card.sass";
 
 const SearchInOffersCards = () => {
+    addCardHandler();
+
+    async function addCardHandler() {
+        const response = await fetch("api/task", {
+            headers: {
+                "Content-Type": "application/all",
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <div className="search-in-offers-cards">
             <RealEstateCard />
